6301a1109d99223336fd59765754e4c0c89faab2
Michael Grantham <MichaelGrantham@live.com>
2021-12-09 18:00:22 -0600

    Initial Commit with working inequality and median income code

---
+++ Inequality and Median Income Code.R
@@ -0,0 +1,59 @@
+library(sf)
+library(spdep)
+library(tigris)
+library(acs)
+library(tidyverse)
+library(tmap)
+library(tidycensus)
+
+"https://towardsdatascience.com/spatial-autocorrelation-neighbors-affecting-neighbors-ed4fab8a4aac"
+"https://www.kaggle.com/stevepalley/2016uspresidentialvotebycounty"
+states <- read.csv("States.csv", header=TRUE)
+cont.states <- read.csv("ContiguousStates.csv", header=TRUE)
+a <- states$Abbreviation
+b <- cont.states$Abbreviation
+
+api="fd79d3eb4ce2c69f2ca5d2ac69199b3930b798ed"
+api.key.install(key=api)
+acs.tables.install()
+set.seed(1234) # because we are randomizing part of the process
+
+# Access the shapefile
+s <- get_acs(key = api, geography = "tract", variables = c("B19013_001", "B19083_001E"), 
+             state = b, geometry = TRUE)
+# remove NA values is any
+s <- na.omit(s)
+s$NAME
+# select column to work with
+s <- subset(s, select=c("estimate"))
+# check data skewness
+hist(s$estimate, main=NULL)
+# check for outliers
+boxplot(s$estimate, horizontal = TRUE)
+# plot variable
+tm_shape(s) + tm_fill(col="estimate", style="quantile", n=5, palette="Greens") +
+  tm_legend(outside=TRUE)
+
+# define neighbor
+nb <- poly2nb(s, queen=TRUE) # here nb list all ID numbers of neighbors;
+# assign weights to neighbors
+lw <- nb2listw(nb, style="W", zero.policy=TRUE) # equal weights
+# compute neighbor average
+inc.lag <- lag.listw(lw, s$estimate)
+# plot polygons vs lags
+plot(inc.lag ~ s$estimate, pch=16, asp=1)
+M1 <- lm(inc.lag ~ s$estimate)
+abline(M1, col="blue")
+# access Moran's coeff
+coef(M1)[2]
+# calculating Moran coeff with one line
+I <- moran(s$estimate, lw, length(nb), Szero(lw))[1]
+
+# hypothesis test with moran.test function
+moran.test(s$estimate,lw, alternative="greater")
+# using Monte-Carlo simulation
+MC<- moran.mc(s$estimate, lw, nsim=999, alternative="greater")
+# View results (including p-value)
+MC
+# plot Null distribution
+plot(MC)
